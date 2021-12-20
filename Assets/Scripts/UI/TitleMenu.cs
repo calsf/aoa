81e01c2e75e34f6259a33c1826d791d321072a3c
calsf <lefrogjones@gmail.com>
2021-12-20 18:27:44 -0500

    starting hud

--- Assets/Scripts/UI/TitleMenu.cs
+++ Assets/Scripts/UI/TitleMenu.cs
@@ -7,20 +7,28 @@ public class TitleMenu : MonoBehaviour
 {
     [SerializeField] private CanvasGroup settingsScreen;
     [SerializeField] private CanvasGroup controlsScreen;
+    [SerializeField] private PlayerStateObject playerState;
 
-    /*
     void Start()
     {
+        // Need to reset time scale in case player quit from pause menu
+        Time.timeScale = 1;
+
+        /*
         // Hide and deactivate screens at start
         settingsScreen.alpha = 0;
         settingsScreen.blocksRaycasts = false;
 
         controlsScreen.alpha = 0;
         controlsScreen.blocksRaycasts = false;
-    }*/
+        */
+    }
 
     public void OnPlay()
     {
+        // Initialize player state on play
+        playerState.InitializeState();
+
         SceneManager.LoadScene("Level001");
     }
 
---
+++ Assets/Scripts/UI/WeaponDisplay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponDisplay : MonoBehaviour
+{
+    [SerializeField] private Text magCurr;
+    [SerializeField] private Text magMax;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
