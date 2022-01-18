3e38af135deb9b2a1130d42895ccfa80ad23029a
calsf <lefrogjones@gmail.com>
2022-01-17 22:37:41 -0500

    add upright model

---
+++ Assets/Scripts/Enemy/EnemyGroundUpright.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroundUpright : EnemyGround
+{
+    [SerializeField] private GameObject head;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    void Update()
+    {
+        
+    }
+}
