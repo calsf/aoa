0e368d6cda7ead1b51839152e1359b95248c8b23
calsf <lefrogjones@gmail.com>
2022-01-19 19:26:01 -0500

    add gunners

---
+++ Assets/Scripts/Enemy/EnemyAirGunner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAirGunner : EnemyAir
+{
+    protected override void Start()
+    {
+        base.Start();
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
